ba15c3ccc8630c9e8812ab4ecd772240e4748faa
m.pinchuk <JoJolkree@gmail.com>
2023-02-27 21:36:06 +0500

    Ревью от 27.02.2023

--- PetProject/Images.cs
+++ PetProject/Images.cs
@@ -1,8 +1,10 @@
 namespace TelegramBotExperiments;
 
+// То же самое, что с Buttons
 public class Images
 {
     private static Dictionary<int, string> imageDictionary = new();
+    // Можно же просто зачитывать все картинки из папки и даже не хранить их в словаре. Ну и картинок нет в репозитории, я не могу собрать и сразу запустить бота
     private static void FillDictionary()
     {
         for(var i = 0; i < 100; i++)
